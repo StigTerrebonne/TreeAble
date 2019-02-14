2493941521374a1fd648a9c4a1b64df49909160a
Stig Terrebonne <stigterrebonne@gmail.com>
2019-02-13 17:28:03 -0800

    Profile page, Gallery, and back buttons

---
+++ public/js/galleryQuery.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+function initializePage() {
+}
+
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
