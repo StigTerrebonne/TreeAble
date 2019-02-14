d4916bf3349f06cb87de4aa0e8a505cddbe0abb3
Stig Terrebonne <stigterrebonne@gmail.com>
2019-02-13 19:23:22 -0800

    Fix how navbar drops, new button on leaderboard/ranking display

--- public/js/leaderboardQuery.js
+++ public/js/leaderboardQuery.js
@@ -8,3 +8,15 @@ $(document).ready(function() {
 function initializePage() {
 	console.log("Javascript connected!");
 }
+
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
