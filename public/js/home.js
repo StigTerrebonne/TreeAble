b7d536874606b6953cfce054cec24ba9921fcf10
immmjimmy <immmjimmy@gmail.com>
2019-02-19 16:55:51 -0800

    refactored entire project with bootstrap 4

--- public/js/home.js
+++ public/js/home.js
@@ -1,36 +1,48 @@
-var options = {
-  chart: {
-      height: 350,
-      type: 'line',
-      zoom: {
-          enabled: false
-      }
-  },
-  dataLabels: {
-      enabled: false
-  },
-  stroke: {
-      curve: 'straight'
-  },
-  series: [{
-      name: "Items recycled",
-      data: [0, 4, 2, 3, 1, 4, 5]
-  }],
-  title: {
-      text: 'Weekly Recycling Progress Chart',
-      align: 'left'
-  },
-  grid: {
-      row: {
-          colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-          opacity: 0.5
-      },
-  },
-  xaxis: {
-      categories: ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'],
-  }
-}
-  
-var chart = new ApexCharts(document.querySelector("#progress-chart"), options);
+'use strict';
 
-chart.render();
+$(document).ready(function() {
+    var options = {
+    chart: {
+        height: 350,
+        type: 'line',
+        zoom: {
+            enabled: false
+        }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    stroke: {
+        curve: 'straight'
+    },
+    series: [{
+        name: "Items recycled",
+        data: [0, 4, 2, 3, 1, 4, 5]
+    }],
+    title: {
+        text: 'Weekly Recycling Progress Chart',
+        align: 'left'
+    },
+    grid: {
+        row: {
+            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+            opacity: 0.5
+        },
+    },
+    xaxis: {
+        categories: ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'],
+    }
+    }
+    
+    var variable = $('#progress-chart');
+    console.log(variable);
+    var chart = new ApexCharts(document.querySelector("#progress-chart"), options);
+
+
+    chart.render().then(function(res) {
+    
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+});
